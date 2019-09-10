9ece7c80bc6c166435ed7d248c7de0057792f9a2
Adan <ajpalacios.sist@gmail.com>
2019-09-10 10:18:02 -0500

    in develop acerca de section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Cube from '../img/undraw_solution_mindset_34bi.svg'
-import Form from './Contact.form';
+import Hero from '../img/hero.svg'
 import ContactForm from './Contact.form';
 
 export default () => {
@@ -11,17 +10,14 @@ export default () => {
         <div className="flex justify-center items-center">
           <div className="flex-1">
             <h1 className="font-bold text-purple-700 text-6xl">¡Hola! soy Adan</h1>
-            <p className="text-xl font-light">
-              Estoy ubicado en 🇲🇽 desde 2016 tengo experiencia profesional como Full Stack Web Developer, 
+            <p className="text-xl font-light text-justify mr-4">
+              Estoy ubicado en Ciudad de Mèxico 🇲🇽, desde 2016 tengo experiencia profesional como Full Stack Web Developer, 
               he pasado por diferentes tecnologias, como PHP, JavaScript y Java enfocado al desarrollo movil, 
-              si les cuento algunos frameworks o librerias, conozco Laravel, React JS ❤️, Vue Js etc...</p>
+              si les cuento algunos frameworks o librerias, conozco Laravel, React JS ❤️, Vue Js etc...
+              </p>
           </div>
-          <p className="text-xl font-light">
-            Tengo un canal de YouTube 📹 en donde te enseño programaciòn 💻 y el maravilloso mundo de la web.
-          </p>
-          <img src={Cube} alt="cube" style={{height: '300px'}}/>
+          <img src={Hero} alt="cube" style={{height: '300px'}}/>
         </div>
-
         <div>
           <ContactForm />
         </div>
